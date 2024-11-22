8868a91e0bf5b6401486df90dbf690bb08ee3825
paxasonik <paxasonk@mail.ru>
2024-11-22 16:20:10 +0300

    added connect to mongodb

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
+const mongoose = require('mongoose');
 
 const PORT = process.env.PORT || 5000;
 const app = express();
@@ -12,7 +13,8 @@ app.use(cors());
 
 const start = async () => {
   try {
-    app.listen(PORT, () => console.log(`Server started on PORT = ${PORT}`))
+    await mongoose.connect(process.env.DB_URL);
+    app.listen(PORT, () => console.log(`Server started on PORT = ${PORT}`));
   } catch (e) {
     console.log(e);
   }
