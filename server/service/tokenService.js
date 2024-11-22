f04b7db7018527e0501a4040b338c99470de8170
paxasonik <paxasonk@mail.ru>
2024-11-22 18:20:12 +0300

    added registration

--- server/service/tokenService.js
+++ server/service/tokenService.js
@@ -1,5 +1,24 @@
-class TokenService {
+const jwt = require('jsonwebtoken');
+const tokenModel = require('../models/tokenModel');
 
+class TokenService {
+  generateTokens(payload) {
+    const accessToken = jwt.sign(payload, process.env.JWT_ACCESS_SECRET, { expiresIn: '15m' });
+    const refreshToken = jwt.sign(payload, process.env.JWT_REFRESH_SECRET, { expiresIn: '30d' });
+    return {
+      accessToken,
+      refreshToken
+    }
+  }
+
+  async saveToken(userId, refreshToken) {
+    const tokenData = await tokenModel.findOne({ user: userId });
+    if (tokenData) {
+      tokenData.refreshToken = refreshToken;
+      return tokenData.save();
+    }
+    return await tokenModel.create({ user: userId, refreshToken });
+  }
 }
 
 module.exports = new TokenService();
--- server/service/userService.js
+++ server/service/userService.js
@@ -1,5 +1,31 @@
-class UserService {
+const UserModel = require('../models/userModel');
+const bcrypt = require('bcrypt');
+const uuid = require('uuid');
+const MailService = require('../service/mailService');
+const TokenService = require('../service/tokenService');
+const UserDto = require('../dtos/userDto');
 
+class UserService {
+  async registration(email, password) {
+    const candidate = await UserModel.findOne({ email });
+    if (candidate) {
+      throw new Error(`Пользователь с почтовым адресом ${email} уже существует`);
+    }
+    const hashPassword = await bcrypt.hash(password, 3);
+    const activationLink = uuid.v4();
+
+    const user = await UserModel.create({ email, password: hashPassword, activationLink });
+    await MailService.sendActivationMail(email, activationLink);
+
+    const userDto = new UserDto(user);
+    const tokens = TokenService.generateTokens({ ...userDto });
+    await TokenService.saveToken(userDto.id, tokens.refreshToken);
+
+    return {
+      ...tokens,
+      user: userDto,
+    }
+  }
 }
 
 module.exports = new UserService();
