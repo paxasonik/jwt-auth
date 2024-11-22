0192a0c83a166a229f309199ae7b92fb83ad88a7
paxasonik <paxasonk@mail.ru>
2024-11-22 17:37:46 +0300

    added project structure

---
+++ server/controllers/userController.js
@@ -0,0 +1,50 @@
+class UserController {
+  async registration(req, res, next) {
+    try {
+
+    } catch (e) {
+
+    }
+  }
+  async login(req, res, next) {
+    try {
+
+    } catch (e) {
+
+    }
+  }
+
+  async logout(req, res, next) {
+    try {
+
+    } catch (e) {
+
+    }
+  }
+
+  async activate(req, res, next) {
+    try {
+
+    } catch (e) {
+
+    }
+  }
+
+  async refresh(req, res, next) {
+    try {
+
+    } catch (e) {
+
+    }
+  }
+
+  async getUsers(req, res, next) {
+    try {
+      res.json(['123', '456']); // test
+    } catch (e) {
+
+    }
+  }
+}
+
+module.exports = new UserController();
--- server/index.js
+++ server/index.js
@@ -3,17 +3,24 @@ const express = require('express');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const mongoose = require('mongoose');
+const router = require('./router/index')
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors());
+app.use('/api', router);
 
 const start = async () => {
   try {
-    await mongoose.connect(process.env.DB_URL);
+    mongoose.connect(process.env.DB_URL)
+      .then(() => {
+        console.log('Connection to MongoDB is successful');
+      }).catch(error => {
+        console.error('Connection error to MongoDB:', error);
+      });
     app.listen(PORT, () => console.log(`Server started on PORT = ${PORT}`));
   } catch (e) {
     console.log(e);
